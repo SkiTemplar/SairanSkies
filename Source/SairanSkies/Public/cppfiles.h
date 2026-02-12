5915968acce63ba284ea1f8c6eaaf7153231ac11
Rodrigo Fernandez <rodrixfc17@gmail.com>
2026-02-12 11:10:35 +0100

    Initial commit

---
+++ Source/SairanSkies/Public/cppfiles.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SAIRANSKIES_API cppfiles
+{
+public:
+	cppfiles();
+	~cppfiles();
+};
